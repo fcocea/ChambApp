979b27198b919b446e24388d9321c656a515c855
Bruno Arce R. <brunoarce0@gmail.com>
2024-10-28 20:46:05 -0300

    Removed headers in chamber

--- src/app/(chamber)/_layout.tsx
+++ src/app/(chamber)/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="menu" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="menu" options={{ headerShown: false }} />
     </Stack>
   );
 }
