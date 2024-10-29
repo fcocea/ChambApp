d4e0907c902fa92b4ab084474d64928242782eac
macamadridb <MAMADRID2020@UDEC.CL>
2024-10-29 01:32:35 -0300

    Botones

--- src/app/(user)/index.tsx
+++ src/app/(user)/index.tsx
@@ -1,29 +1,26 @@
-import { Button, Text, View } from "react-native";
+import { Pressable, ScrollView, Text, View } from "react-native";
 
-import { useAuth } from "../../hooks/useAuth";
+// import { useAuth } from "../../hooks/useAuth";
 
 export default function Index() {
-  const { setAuthState } = useAuth();
+  // const { setAuthState } = useAuth();
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="font-bold">User View </Text>
-      <Button
-        onPress={() => {
-          setAuthState({
-            mode: "chamber"
-          });
-        }}
-        // className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        title="Modo Chamber"
-      >
-      </Button>
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="font-bold">Hola Juan</Text>
+      <View className="flex flex-row gap-8">
+        <Pressable className="rounded-[12px] bg-[#2F80ED] px-4 p-2 ">
+          <Text className="font-bold text-white">Tus anuncios</Text>
+        </Pressable>
+        <Pressable className="rounded-[12px] bg-[#2F80ED] px-4 p-2 ">
+          <Text className="font-bold text-white">Historial</Text>
+        </Pressable>
+        <Pressable className="rounded-[25px] bg-[#2F80ED] px-4 p-2 ">
+          <Text className="font-bold text-white">+</Text>
+        </Pressable>
+      </View>
+      <ScrollView>
 
-      <Button
-        onPress={() => {
-          setAuthState(null);
-        }}
-        title="Logout"
-      />
+      </ScrollView>
     </View>
   );
 }
