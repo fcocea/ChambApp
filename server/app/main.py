e23f2356a3986b441732fb7eb0658f6e826dc6a6
Luis Valenzuela <lvalenzuel2020@udec.cl>
2024-11-10 18:22:20 -0300

    fix: database connection

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_versioning import VersionedFastAPI
 from app.routers import users, advertisement
 
 app = FastAPI()
+
+origins = ["*"]
 
 app.include_router(users.router)
 app.include_router(advertisement.router)
@@ -16,3 +19,5 @@ def read_root():
 app = VersionedFastAPI(
     app, version="1.0", prefix_format="/v{major}", enable_latest=False
 )
+
+app.add_middleware(CORSMiddleware, allow_origins=origins)
