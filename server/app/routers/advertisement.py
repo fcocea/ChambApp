8a3b393c414ced5f7be81b32db7a20c9919f1e99
Luis Valenzuela <lvalenzuel2020@udec.cl>
2024-11-11 20:23:37 -0300

    get user advertisements by header token

--- server/app/routers/advertisement.py
+++ server/app/routers/advertisement.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, HTTPException, Query, Header
+from fastapi import APIRouter, HTTPException, Header
 from fastapi_versioning import version
-from typing import List, Optional, Annotated
+from typing import List, Annotated
 from uuid import UUID
 from app.core.database import connect_to_db, close_db_connection
 import jwt
@@ -10,81 +10,48 @@ from pydantic import BaseModel
 from datetime import datetime
 
 router = APIRouter(
-    prefix="/advertisement",
+    prefix="/advertisements",
 )
 
 
-@router.get("/", response_model=List[dict])
+@router.get("", response_model=List[dict])
 @version(1)
 async def get_advertisements(
-    area_id: Optional[int] = Query(default=None, description="Area ID"),
-    status: Optional[int] = Query(default=None, description="Advertisement status"),
+    Authorization: Annotated[str | None, Header(convert_underscores=False)] = None,
 ):
+    if not Authorization:
+        raise HTTPException(status_code=400, detail="No access token provided")
+    payload = jwt.decode(Authorization, SECRET_KEY, algorithms=[ALGORITHM])
+    rut = payload["rut"]
     connection = await connect_to_db()
     try:
         query = """
-            SELECT 
-                ad.ad_id, 
-                ad.title, 
-                ad.description, 
-                ad.creation_date, 
-                ad.status,
+            SELECT ad.ad_id,
+                ad.title,
+                ad.description,
                 ad.price,
                 ad.start_date,
                 ad.address,
-                u.rut,
+                ad.status,
+                array_agg(ar.name) AS area_names,
                 u.first_name,
                 u.last_name
-            FROM 
-                "Advertisement" ad
-            INNER JOIN 
-                "AdvertisementArea" aa ON ad.ad_id = aa.ad_id
-            INNER JOIN 
-                "Area" a ON aa.area_id = a.area_id
-            INNER JOIN 
-                "User" u ON ad.created_by = u.rut 
-            """
-        conditions = []
-        params = []
-        if area_id is not None:
-            params.append(area_id)
-            conditions.append(f"aa.area_id = ${len(params)}")
-        if status is not None:
-            params.append(status)
-            conditions.append(f"ad.status = ${len(params)}")
-        if conditions:
-            query += " WHERE " + " AND ".join(conditions)
+            FROM "Advertisement" AS ad
+            JOIN "AdvertisementArea" AS aa ON ad.ad_id = aa.ad_id
+            JOIN "User" AS u ON ad.created_by = u.rut
+            JOIN "Area" AS ar ON aa.area_id = ar.area_id
+            WHERE aa.area_id IN (
+                    SELECT cs.area_id
+                    FROM "ChamberSpecialize" AS cs
+                    WHERE cs.rut = $1
+                )
+                AND ad.created_by != $1
+                AND ad.status = 0
+            GROUP BY ad.ad_id, u.first_name, u.last_name;
+        """
+        advertisements = await connection.fetch(query, rut)
 
-        query += """
-            GROUP BY ad.ad_id, ad.title, ad.description, ad.creation_date, 
-            ad.status, ad.price, ad.start_date, ad.address, 
-            u.rut, u.first_name, u.last_name;
-            """
-
-        rows = await connection.fetch(query, *params)
-        if not rows:
-            raise HTTPException(status_code=404, detail="No advertisements found.")
-
-        advertisements = [
-            {
-                "ad_id": row["ad_id"],
-                "title": row["title"],
-                "description": row["description"],
-                "creation_date": row["creation_date"],
-                "status": row["status"],
-                "price": row["price"],
-                "start_date": row["start_date"],
-                "address": row["address"],
-                "created_by": {
-                    "rut": row["rut"],
-                    "first_name": row["first_name"],
-                    "last_name": row["last_name"],
-                },
-            }
-            for row in rows
-        ]
-
-        return advertisements
+        return [dict(advertisement) for advertisement in advertisements]
 
     finally:
         await close_db_connection(connection)
@@ -128,7 +95,7 @@ async def get_advertisement_info(ad_id: UUID):
         await close_db_connection(connection)
 
 
-@router.get("/{ad_id}/applications/", response_model=List[dict])
+@router.get("/{ad_id}/applications", response_model=List[dict])
 @version(1)
 async def get_advertisement_applications(ad_id: UUID):
     connection = await connect_to_db()
@@ -193,7 +160,7 @@ async def get_advertisement_applications(ad_id: UUID):
         await close_db_connection(connection)
 
 
-@router.post("/{ad_id}/apply/", response_model=dict)
+@router.post("/{ad_id}/apply", response_model=dict)
 @version(1)
 async def advertisement_apply(
     ad_id: UUID,
@@ -209,6 +176,9 @@ async def advertisement_apply(
             SELECT created_by FROM "Advertisement" WHERE ad_id = $1
         """
         ad_owner = await connection.fetchval(check_query, ad_id)
+
+        if not ad_owner:
+            raise HTTPException(status_code=400, detail="Advertisement does not exist")
 
         if ad_owner == rut:
             raise HTTPException(
