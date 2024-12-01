453b76368668e312855b68dfbc767cd9e8fa5004
Francisco Cea <fcoceak@gmail.com>
2024-12-01 16:56:56 -0300

    Refactor get_my_advertisements endpoint to retrieve advertisements created by the user

--- src/app/(user)/(advertisement)/advertisement/[id]/review.tsx
+++ src/app/(user)/(advertisement)/advertisement/[id]/review.tsx
@@ -1,5 +1,5 @@
 import { useContext, useMemo } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Svg, {
   Ellipse,
@@ -114,81 +114,83 @@ export default function AdvertisementReview() {
         <AdvertisementLocation />
       </View>
       <Separator color="#E5E7EB" />
-      <View className="flex gap-3">
-        <Text className="text-[#333333] text-lg font-bold">{advertisementData.info?.title}</Text>
-        <View className="flex flex-row justify-between">
-          <View className="flex flex-1 flex-col justify-between">
-            <View className="flex flex-row items-center gap-1">
-              <Feather name="calendar" size={18} color="black" />
-              <Text className="text-base">{new Date(advertisementData?.info?.start_date || new Date()).toLocaleDateString("es-CL", { year: "numeric", month: "long", day: "numeric" })}</Text>
-            </View>
-            <View className="flex flex-row items-center gap-1">
-              <Feather name="clock" size={18} color="black" />
-              <Text className="text-base">{new Date(advertisementData?.info?.start_date || new Date()).toLocaleTimeString("es-CL", { hour: "2-digit", minute: "2-digit" })}</Text>
-            </View>
-          </View>
-
-          <View className="flex flex-row gap-1 justify-center">
-            <View className="flex flex-col justify-center items-end gap-1">
-              <Text className="text-base">
-                {advertisementData.selected?.first_name}
-                {" "}
-                {advertisementData.selected?.last_name}
-              </Text>
-              <View>
-                <Text className="text-base">
-                  {Number(advertisementData.selected?.average_score || 0).toFixed(1)}
-                </Text>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName="gap-5 pb-8" alwaysBounceVertical={false}>
+        <View className="flex gap-3">
+          <Text className="text-[#333333] text-lg font-bold">{advertisementData.info?.title}</Text>
+          <View className="flex flex-row justify-between">
+            <View className="flex flex-1 flex-col justify-between">
+              <View className="flex flex-row items-center gap-1">
+                <Feather name="calendar" size={18} color="black" />
+                <Text className="text-base">{new Date(advertisementData?.info?.start_date || new Date()).toLocaleDateString("es-CL", { year: "numeric", month: "long", day: "numeric" })}</Text>
+              </View>
+              <View className="flex flex-row items-center gap-1">
+                <Feather name="clock" size={18} color="black" />
+                <Text className="text-base">{new Date(advertisementData?.info?.start_date || new Date()).toLocaleTimeString("es-CL", { hour: "2-digit", minute: "2-digit" })}</Text>
               </View>
             </View>
-            <Avatar size={48} name={advertisementData.selected?.first_name + " " + advertisementData.selected?.last_name} />
+
+            <View className="flex flex-row gap-1 justify-center">
+              <View className="flex flex-col justify-center items-end gap-1">
+                <Text className="text-base">
+                  {advertisementData.selected?.first_name}
+                  {" "}
+                  {advertisementData.selected?.last_name}
+                </Text>
+                <View>
+                  <Text className="text-base">
+                    {Number(advertisementData.selected?.average_score || 0).toFixed(1)}
+                  </Text>
+                </View>
+              </View>
+              <Avatar size={48} name={advertisementData.selected?.first_name + " " + advertisementData.selected?.last_name} />
+            </View>
           </View>
         </View>
-      </View>
-      <Separator color="#E5E7EB" />
-      <View className="flex flex-col gap-2">
-        <Text className="font-medium">Precio</Text>
-        <View className="flex flex-col pl-1">
+        <Separator color="#E5E7EB" />
+        <View className="flex flex-col gap-2">
+          <Text className="font-medium">Precio</Text>
+          <View className="flex flex-col pl-1">
+            <View className="flex justify-between w-full flex-row">
+              <Text className="text-base">Servicio</Text>
+              <Text className="text-base font-light">{formatMoney(advertisementData.info?.price || 0)}</Text>
+            </View>
+            <View className="flex justify-between w-full flex-row">
+              <Text className="text-base">Tarifa de servicio</Text>
+              <Text className="text-base font-light">{formatMoney(taxService)}</Text>
+            </View>
+            <View className="flex justify-between w-full flex-row">
+              <Text className="text-base">IVA</Text>
+              <Text className="text-base font-light">{formatMoney(iva)}</Text>
+            </View>
+          </View>
+          <Separator className="px-8" color="#E5E7EB" />
           <View className="flex justify-between w-full flex-row">
-            <Text className="text-base">Servicio</Text>
-            <Text className="text-base font-light">{formatMoney(advertisementData.info?.price || 0)}</Text>
-          </View>
-          <View className="flex justify-between w-full flex-row">
-            <Text className="text-base">Tarifa de servicio</Text>
-            <Text className="text-base font-light">{formatMoney(taxService)}</Text>
-          </View>
-          <View className="flex justify-between w-full flex-row">
-            <Text className="text-base">IVA</Text>
-            <Text className="text-base font-light">{formatMoney(iva)}</Text>
+            <Text className="font-medium text-base">Total</Text>
+            <Text>
+              {formatMoney(iva + taxService + (advertisementData.info?.price || 0))}
+            </Text>
           </View>
         </View>
-        <Separator className="px-8" color="#E5E7EB" />
-        <View className="flex justify-between w-full flex-row">
-          <Text className="font-medium text-base">Total</Text>
-          <Text>
-            {formatMoney(iva + taxService + (advertisementData.info?.price || 0))}
-          </Text>
+        <Separator color="#E5E7EB" />
+        <View className="flex justify-between flex-row items-center">
+          <Text className="text-base font-medium">Pago</Text>
+          <View className="flex flex-row gap-2 items-center">
+            <Feather name="credit-card" size={18} color="black" />
+            <Text className="text-base text-[#333333]">...7896</Text>
+            <Feather name="chevron-right" size={18} color="#333333" />
+          </View>
         </View>
-      </View>
-      <Separator color="#E5E7EB" />
-      <View className="flex justify-between flex-row items-center">
-        <Text className="text-base font-medium">Pago</Text>
-        <View className="flex flex-row gap-2 items-center">
-          <Feather name="credit-card" size={18} color="black" />
-          <Text className="text-base text-[#333333]">...7896</Text>
-          <Feather name="chevron-right" size={18} color="#333333" />
-        </View>
-      </View>
-      <Separator color="#E5E7EB" />
-      <Text className="text-xs opacity-40 text-[#333333] font-light">
-        {/* Disclaimer text */}
-        * Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nunc nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nunc nec nunc.
-        {" "}
-        {id}
-      </Text>
-      <TouchableOpacity className="w-full py-4 px-3 bg-primary rounded-xl mt-auto mb-4">
-        <Text className="text-white text-center text-base font-medium">Confirmar y chatear</Text>
-      </TouchableOpacity>
+        <Separator color="#E5E7EB" />
+        <Text className="text-xs opacity-40 text-[#333333] font-light">
+          {/* Disclaimer text */}
+          * Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nunc nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nunc nec nunc.
+          {" "}
+          {id}
+        </Text>
+        <TouchableOpacity className="w-full py-4 px-3 bg-primary rounded-xl mt-auto mb-4">
+          <Text className="text-white text-center text-base font-medium">Confirmar y chatear</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
--- src/app/(user)/(tabs)/index.tsx
+++ src/app/(user)/(tabs)/index.tsx
@@ -1,42 +1,62 @@
 import { useEffect, useState } from "react";
-import { Pressable, ScrollView, Text, View } from "react-native";
+import { Pressable, RefreshControl, ScrollView, Text, View } from "react-native";
 import { Dimensions } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Feather } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import { useRouter } from "expo-router";
-import { ArrowRight, Bell, ChevronDown, Plus } from "lucide-react-native";
+import { ArrowRight, Bell, ChevronDown, Eye, Plus } from "lucide-react-native";
 
 import { Skeleton } from "@/components/ui/Skeleton";
 import { useAuth } from "@/hooks/useAuth";
 
 const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+const API_ENDPOINT = process.env.EXPO_PUBLIC_API_URL;
 
-const AdvertisementCard = () => {
+const AdvertisementCard = ({ data }: { data: any }) => {
   const router = useRouter();
   return (
-    <Pressable className="flex bg-white rounded-2xl overflow-hidden w-full" style={{ width: (screenWidth - 48) - 4 }} onPress={() => router.push("./(advertisement)/advertisement/beed073c-d0b9-4e00-a3ce-a815135c464c/select")}>
+    <Pressable className="flex bg-white rounded-2xl overflow-hidden w-full" style={{ width: (screenWidth - 48) - 4 }} onPress={() => router.push(`./(advertisement)/advertisement/${data?.ad_id}/select`)}>
       <Image source={{ uri: "https://i.pinimg.com/564x/b7/21/3d/b7213d2e2ca6435c504bfd4294c86288.jpg" }} style={{ width: "100%", height: 180 }} />
       <View className="flex flex-row justify-between absolute right-1 top-2 gap-2">
-        <View className="bg-[#FAFAFA] rounded-md px-2 py-1">
-          <Text className="text-xs">
-            Categoria
-          </Text>
-        </View>
-        <View className="bg-[#FAFAFA] rounded-md px-2 py-1">
-          <Text className="text-xs">
-            Categoria
-          </Text>
-        </View>
+        {data?.areas?.slice(0, 2)?.map((area: string, index: number) => (
+          <View key={index} className="bg-[#FAFAFA] rounded-md px-2 py-1">
+            <Text className="text-xs">
+              {area}
+            </Text>
+          </View>
+        ))}
       </View>
       <View className="flex flex-col p-4">
         <View className="flex flex-row justify-between items-center">
-          <Text className="font-semibold text-[#333] text-lg">Anuncio 1</Text>
-          <View className="bg-primary rounded-md px-2 py-1">
-            <Text className="text-xs text-white">En progreso</Text>
-          </View>
+          <Text className="font-semibold text-[#333] text-lg">{data?.title}</Text>
+          {data?.status === 1
+            ? (
+                <View className="bg-primary rounded-md px-2 py-1">
+                  <Text className="text-xs text-white">En progreso</Text>
+                </View>
+              )
+            : <Text className="text-sm text-[#50647D]">{new Intl.NumberFormat("es-CL", { style: "currency", currency: "CLP" }).format(data?.price)}</Text>}
         </View>
-        <Text className="text-sm text-[#50647D]">{new Date(new Date()).toLocaleDateString("es-CL", { year: "numeric", month: "long", day: "numeric", hour: "2-digit", minute: "2-digit" })}</Text>
+        {
+          data?.status === 1
+            ? (
+                <Text className="text-sm text-[#50647D]">{new Date(data?.start_date).toLocaleDateString("es-CL", { year: "numeric", month: "long", day: "numeric", hour: "2-digit", minute: "2-digit" })}</Text>
+              )
+            : (
+                <View className="flex flex-row items-center gap-1">
+                  <Eye color="#50647D" size={16} />
+                  <Text className="text-sm text-[#50647D]">
+                    <Text className="text-primary font-semibold">
+                      {data?.total_applications}
+                      {" "}
+                    </Text>
+                    chambers postulando
+                  </Text>
+                </View>
+              )
+        }
       </View>
     </Pressable>
   );
@@ -49,7 +69,7 @@ const AdvertisementCardSkeleton = () => (
       <View className="flex flex-row justify-between items-center">
         <Skeleton width={160} height={16} />
       </View>
-      <Skeleton width={190} height={10} />
+      <Skeleton width={190} height={14} />
     </View>
   </View>
 );
@@ -84,20 +104,43 @@ const AdvertisementHistoryCardSkeleton = () => (
 
 export default function Index() {
   const insets = useSafeAreaInsets();
-  const { logout } = useAuth();
-  const [loading, setLoading] = useState<boolean>(true);
+  const { authState, logout } = useAuth();
   const router = useRouter();
+  const [advertisements, setAdvertisements] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+
+  const fetchAdvertisements = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`${API_ENDPOINT}/users/me/advertisements`, {
+        headers: {
+          Authorization: `Bearer ${authState?.token}`
+        }
+      });
+      const data = await response.json();
+      setAdvertisements(data);
+    } catch (error) {
+      console.error("Error fetching advertisements:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchAdvertisements();
+    setRefreshing(false);
+  };
 
   useEffect(() => {
-    (async () => {
-      await new Promise(resolve => setTimeout(resolve, 5000));
-      setLoading(false);
-    })();
+    fetchAdvertisements();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <View className="flex-1 bg-primary h-full w-full" style={{ paddingTop: insets.top }}>
-      <View className="px-6 flex flex-col gap-8 pt-1">
+    <View className="bg-primary w-full" style={{ paddingTop: insets.top, height: screenHeight }}>
+      <View className="px-6 flex flex-col gap-8 py-2">
         <View className="flex-row items-center justify-between">
           <View className="flex flex-row gap-2 items-center">
             <Text className="text-white text-base font-semibold justify-center ">
@@ -107,74 +150,91 @@ export default function Index() {
           </View>
           <Pressable
             className="flex flex-row items-center rounded-full border-[#FAFAFA] border-[1px] p-2"
-            onPress={() => {
-              logout();
-            }}
+            onPress={logout}
           >
             <Bell size={18} color="white" />
             <View className="bg-red-500 rounded-full w-2 h-2 absolute top-1 right-[9px]" />
           </Pressable>
         </View>
-        <View>
+      </View>
+      <ScrollView
+        className="bg-primary"
+        refreshControl={(
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={["#1B456D"]}
+            progressBackgroundColor="#fff"
+            tintColor="#FFF"
+            title="Actualizando..."
+          />
+        )}
+        contentContainerClassName="flex-1 h-full"
+        showsVerticalScrollIndicator={false}
+        alwaysBounceVertical={false}
+        // bounces={false}
+      >
+        <View className="px-6 mt-10 mb-20">
           <Text className="text-white text-3xl font-semibold">Qué necesitai hacer 🤙?</Text>
           <Text className="text-sm text-gray-300">Encuentra la solución a tus problemas</Text>
         </View>
-      </View>
-      <View className="absolute right-1/2 translate-x-1/2 top-[-45px] h-20 w-20 rounded-full bg-primary" />
-      <View className="flex flex-col bg-[#FAFAFA] rounded-t-[32px] mt-16 pt-6 gap-6 h-full flex-1">
-        <View
-          className="absolute top-[-25px] rounded-t-[1000px] bg-[#FAFAFA]"
-          style={{
-            width: screenWidth * 1.6,
-            height: screenWidth * 0.6,
-            left: -((screenWidth * 1.6 - screenWidth) / 2)
-          }}
-        />
-        <View className="absolute right-1/2 translate-x-1/2 top-[-50px] h-20 w-20 rounded-full bg-primary" />
-        <Pressable className="absolute right-1/2 translate-x-1/2 top-[-42px] h-16 w-16 rounded-full bg-white items-center justify-center" onPress={() => router.push("/(user)/(advertisement)/create")}>
-          <Plus size={28} color="#1b456d" />
-        </Pressable>
-        <View className="flex flex-col px-6 gap-6 flex-grow">
-          <View className="flex flex-col gap-5 w-full flex-1">
-            <View className="flex flex-row justify-between w-full items-center">
-              <Text className="text-2xl text-[#50647D] font-semibold">Anuncios activos</Text>
-              {/* <View className="flex flex-row items-center gap-1">
-                <Text className="text-[#333] font-semibold text-base">Ver todos</Text>
-                <ArrowRight size={18} color="#333" />
-              </View> */}
-            </View>
-            <View className="flex-grow">
-              <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ gap: 8 }} pagingEnabled className="w-full">
+        {/* Contenido principal */}
+        <View className="flex flex-col bg-[#FAFAFA] rounded-t-[32px] pt-6 gap-6 h-full">
+          <View
+            className="absolute top-[-25px] rounded-t-[1000px] bg-[#FAFAFA]"
+            style={{
+              width: screenWidth * 1.6,
+              height: screenWidth * 0.6,
+              left: -((screenWidth * 1.6 - screenWidth) / 2)
+            }}
+          />
+          <View className="absolute right-1/2 translate-x-1/2 top-[-50px] h-20 w-20 rounded-full bg-primary" />
+          <Pressable className="absolute right-1/2 translate-x-1/2 top-[-42px] h-16 w-16 rounded-full bg-white items-center justify-center" onPress={() => router.push("/(user)/(advertisement)/create")}>
+            <Plus size={28} color="#1b456d" />
+          </Pressable>
+          <View className="px-6 flex flex-col gap-6">
+            <View className="flex flex-col gap-5">
+              <View className="flex flex-row justify-between items-center">
+                <Text className="text-2xl text-[#50647D] font-semibold">Anuncios activos</Text>
+              </View>
+              <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ gap: 8 }}
+                pagingEnabled
+              >
                 {loading
                   ? Array.from({ length: 2 }).map((_, index) => (
                     <AdvertisementCardSkeleton key={index} />
                   ))
-                  : Array.from({ length: 3 }).map((_, index) => (
-                    <AdvertisementCard key={index} />
+                  : advertisements.map((data, index) => (
+                    <AdvertisementCard key={index} data={data} />
                   ))}
               </ScrollView>
             </View>
-          </View>
-          <View className="flex flex-col gap-5 w-full flex-1">
-            <View className="flex flex-row justify-between w-full items-center">
-              <Text className="text-2xl text-[#50647D] font-semibold">Historial de anuncios</Text>
-              <Pressable className="flex flex-row items-center gap-1">
-                <Text className="text-[#333333] font-semibold text-base">Ver todos</Text>
-                <ArrowRight size={18} color="#333333" />
-              </Pressable>
+            {/* Historial de anuncios */}
+            <View className="flex flex-col gap-5">
+              <View className="flex flex-row justify-between items-center">
+                <Text className="text-2xl text-[#50647D] font-semibold">Ultimos anuncios</Text>
+                <Pressable className="flex flex-row items-center gap-1">
+                  <Text className="text-[#333333] font-semibold text-base">Ver todos</Text>
+                  <ArrowRight size={18} color="#333333" />
+                </Pressable>
+              </View>
+              {/* Contenedor del ScrollView limitado */}
+              <View className="flex-grow flex gap-2">
+                {loading
+                  ? Array.from({ length: 3 }).map((_, index) => (
+                    <AdvertisementHistoryCardSkeleton key={index} />
+                  ))
+                  : Array.from({ length: 3 }).map((_, index) => (
+                    <AdvertisementHistoryCard key={index} status="active" />
+                  ))}
+              </View>
             </View>
-            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ gap: 8 }}>
-              {loading
-                ? Array.from({ length: 5 }).map((_, index) => (
-                  <AdvertisementHistoryCardSkeleton key={index} />
-                ))
-                : Array.from({ length: 2 }).map((_, index) => (
-                  <AdvertisementHistoryCard key={index} status="active" />
-                ))}
-            </ScrollView>
           </View>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
